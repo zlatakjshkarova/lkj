3a49417c033ab2f0591a537bdbf2d7ef00ef5bd3
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-12-13 22:33:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+1
+let a = [4, 5, 6];
+console.log(a)
+2
+let q = ['wer', 'wsed', 'wesdc'];
+console.log(q);
+3
+let arr = ['a', 'b', 'c', 'd'];
+let edf = arr = [`${"a+b+c+d"}`]
+console.log(edf);
+4
+let clor = ['yellow', 'red', 'black',]
+
+console.log(clor.length);
+
+5
+
+let w = ['hat', 'kiko', 'ioii', 'huj8']
+
+console.log(w[w.length - 1]);
+6
+
+let wqaz = ['a', 'd', 'c'];
+let xd = wqaz = ['1', '2', '3'];
+console.log(xd);
+
+7
+let asd = [1, 2, 3];
+let sde = asd = [1 + 3, 2 + 3, 3 + 3];
+console.log(sde);
+
+let wer = [3, 4, 5];
+8
+
+9
+//for (i = 0; i < quc.push(20);) {
+// if (i % 20 == 0) {
+//console.log(i)
+//  break;
+// } else {
+
+//   }
+//}//
+
+
+10
+11
+let sdf = ['sdf', 'dfgi', 'fgt'];
+console.log(sdf[sdf.length + 1]);
+for (let i = 0; i < wer.length; ++i) {
+
+    console.log(wer[i]);
+    break;
+}
+13
+let arr2 = [2, 4, 5, 6];
+let res = arr2.includes(3);
+console.log(res);
+
+14
+let arr3 = [1, 2, 3];
+let ad = arr3 = [1 + 2 + 3];
+console.log(ad)
